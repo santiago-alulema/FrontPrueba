0a72d457e15fbf80c9196dacbc42c24069184339
santiago-alulema <100087709+santiago-alulema@users.noreply.github.com>
2022-04-25 14:15:54 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -22,7 +22,8 @@ const nextConfig = {
       'api.radimercado.com',
       'source.unsplash.com',
       '45.177.127.116',
-      'dev-apiwise.radimercado.com'
+      'dev-apiwise.radimercado.com',
+      'dev-apiwise.radimercado.com:6607'
     ],
   },
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
